db27f3d540f9c2e55f73e5176a7ec17f8007a313
Broca <z502723158@gmail.com>
2019-11-20 21:59:35 +0800

    test

--- club_manager/src/control/AssoManager.java
+++ club_manager/src/control/AssoManager.java
@@ -1,25 +1,57 @@
 package control;
 
 import java.util.*;
+//import model;
+
+import model.Association;
 
 public class AssoManager {
-   public int joinAsso() {
-      // TODO: implement
-      return 0;
-   }
-   
-   public int exitAsso() {
-      // TODO: implement
-      return 0;
-   }
-   
-   public int assoMemberList() {
-      // TODO: implement
-      return 0;
-   }
-   
-   public int checkExitAsso() {
-      // TODO: implement
-      return 0;
-   }
+
+	// method that asso need
+	public Association modAsso(Association asso) {
+		// TODO: implement
+		
+		return asso;
+	}
+
+	public Association addAsso(Association asso) {
+		// TODO: implement
+		java.sql.Connection conn = null;
+		return asso;
+	}
+
+	public int delAsso() {
+		// TODO: implement
+		return 0;
+	}
+
+	public int assoList() {
+		// TODO: implement
+		return 0;
+	}
+
+	public int searchAsso() {
+		// TODO: implement
+		return 0;
+	}
+  // method need student and asso
+	public int joinAsso() {
+		// TODO: implement
+		return 0;
+	}
+
+	public int exitAsso() {
+		// TODO: implement
+		return 0;
+	}
+
+	public int assoMemberList() {
+		// TODO: implement
+		return 0;
+	}
+
+	public int checkExitAsso() {
+		// TODO: implement
+		return 0;
+	}
 }
